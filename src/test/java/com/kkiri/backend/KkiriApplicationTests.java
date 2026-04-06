f335e537bc7f14c4432d916f1629f329be040331
hi4579675 <hi4579675@gmail.com>
2026-04-06 23:19:37 +0900

    ci: CI 테스트 용 application.yml 작성

--- src/test/java/com/kkiri/backend/KkiriApplicationTests.java
+++ src/test/java/com/kkiri/backend/KkiriApplicationTests.java
@@ -2,8 +2,10 @@ package com.kkiri.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KkiriApplicationTests {
 
 	@Test
